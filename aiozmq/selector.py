aebb03b93c91c52766326be0bea542e884a29b7d
Oleg Höfling <oleg.hoefling@gmail.com>
2020-01-30 12:25:36 +0100

    add type hints for aiozmq.selector
    Signed-off-by: Oleg Höfling <oleg.hoefling@gmail.com>

--- aiozmq/selector.py
+++ aiozmq/selector.py
@@ -1,23 +1,36 @@
 """ZMQ pooler for asyncio."""
 import math
+import sys
 from collections.abc import Mapping
 from errno import EINTR
+from selectors import EVENT_READ, EVENT_WRITE, BaseSelector, SelectorKey
+from typing import Any, Dict, Iterator, List, Mapping, Optional, Tuple, Union
 
-from zmq import (ZMQError, POLLIN, POLLOUT, POLLERR,
-                 Socket as ZMQSocket, Poller as ZMQPoller)
-
+from zmq import POLLERR, POLLIN, POLLOUT
+from zmq import Poller as ZMQPoller
+from zmq import Socket as ZMQSocket
+from zmq import ZMQError
 
 __all__ = ['ZmqSelector']
 
 
-try:
-    from asyncio.selectors import (BaseSelector, SelectorKey,
-                                   EVENT_READ, EVENT_WRITE)
-except ImportError:  # pragma: no cover
-    from selectors import BaseSelector, SelectorKey, EVENT_READ, EVENT_WRITE
 
 
-def _fileobj_to_fd(fileobj):
+if sys.version_info >= (3, 8):
+    from typing import Protocol
+else:
+    from typing_extensions import Protocol
+
+
+class _HasFileno(Protocol):
+    def fileno(self) -> int: ...
+
+_FileObject = Union[int, _HasFileno]
+_FileDescriptor = int
+_EventMask = int
+
+
+def _fileobj_to_fd(fileobj: _FileObject) -> Union[_FileDescriptor, ZMQSocket]:
     """Return a file descriptor from a file object.
 
     Parameters:
@@ -44,37 +57,37 @@ def _fileobj_to_fd(fileobj):
     return fd
 
 
-class _SelectorMapping(Mapping):
+class _SelectorMapping(Mapping[_FileObject, SelectorKey]):
     """Mapping of file objects to selector keys."""
 
-    def __init__(self, selector):
+    def __init__(self, selector: 'ZmqSelector') -> None:
         self._selector = selector
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._selector._fd_to_key)
 
-    def __getitem__(self, fileobj):
+    def __getitem__(self, fileobj: _FileObject) -> SelectorKey:
         try:
             fd = self._selector._fileobj_lookup(fileobj)
             return self._selector._fd_to_key[fd]
         except KeyError:
             raise KeyError("{!r} is not registered".format(fileobj)) from None
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[_FileObject]:
         return iter(self._selector._fd_to_key)
 
 
 class ZmqSelector(BaseSelector):
     """A selector that can be used with asyncio's selector base event loops."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         # this maps file descriptors to keys
-        self._fd_to_key = {}
+        self._fd_to_key = {}  # type: Dict[_FileObject, SelectorKey]
         # read-only mapping returned by get_map()
         self._map = _SelectorMapping(self)
         self._poller = ZMQPoller()
 
-    def _fileobj_lookup(self, fileobj):
+    def _fileobj_lookup(self, fileobj: _FileObject) -> _FileDescriptor:
         """Return a file descriptor from a file object.
 
         This wraps _fileobj_to_fd() to do an exhaustive search in case
@@ -93,7 +106,7 @@ class ZmqSelector(BaseSelector):
             # Raise ValueError after all.
             raise
 
-    def register(self, fileobj, events, data=None):
+    def register(self, fileobj: _FileObject, events: _EventMask, data: Any = None) -> SelectorKey:
         if (not events) or (events & ~(EVENT_READ | EVENT_WRITE)):
             raise ValueError("Invalid events: {!r}".format(events))
 
@@ -116,7 +129,7 @@ class ZmqSelector(BaseSelector):
         self._fd_to_key[key.fd] = key
         return key
 
-    def unregister(self, fileobj):
+    def unregister(self, fileobj: _FileObject) -> SelectorKey:
         try:
             key = self._fd_to_key.pop(self._fileobj_lookup(fileobj))
         except KeyError:
@@ -128,7 +141,7 @@ class ZmqSelector(BaseSelector):
             raise OSError(exc.errno, exc.strerror) from exc
         return key
 
-    def modify(self, fileobj, events, data=None):
+    def modify(self, fileobj: _FileObject, events: _EventMask, data: Any = None) -> SelectorKey:
         try:
             fd = self._fileobj_lookup(fileobj)
             key = self._fd_to_key[fd]
@@ -151,14 +164,14 @@ class ZmqSelector(BaseSelector):
         self._fd_to_key[key.fd] = key
         return key
 
-    def close(self):
+    def close(self) -> None:
         self._fd_to_key.clear()
         self._poller = None
 
-    def get_map(self):
+    def get_map(self) -> Mapping[_FileObject, SelectorKey]:
         return self._map
 
-    def _key_from_fd(self, fd):
+    def _key_from_fd(self, fd: _FileObject) -> Optional[SelectorKey]:
         """Return the key associated to a given file descriptor.
 
         Parameters:
@@ -172,7 +185,7 @@ class ZmqSelector(BaseSelector):
         except KeyError:
             return None
 
-    def select(self, timeout=None):
+    def select(self, timeout: Optional[float] = None) -> List[Tuple[SelectorKey, _EventMask]]:
         if timeout is None:
             timeout = None
         elif timeout <= 0:
@@ -182,7 +195,7 @@ class ZmqSelector(BaseSelector):
             # zero to wait *at least* timeout seconds.
             timeout = math.ceil(timeout * 1e3)
 
-        ready = []
+        ready = []  # type: List[Tuple[SelectorKey, _EventMask]]
         try:
             z_events = self._poller.poll(timeout)
         except ZMQError as exc:
